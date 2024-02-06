4b565113dc79c6c0651b8ec078301f0b05cbfcfd
Ed Dao <ed@intellogic.com>
2024-02-06 09:05:02 -0800

    update journal Stat

--- journal/event.go
+++ journal/event.go
@@ -11,13 +11,29 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+type EventStat struct {
+	Key  string `bson:"key"`
+	Info string `bson:"info, omitempty"`
+}
+
+type EventDocument struct {
+	ID           primitive.ObjectID     `bson:"_id,omitempty"`
+	SessionId    primitive.ObjectID     `bson:"session_id"`
+	TargetId     primitive.ObjectID     `bson:"target_id"`
+	Statistics   []EventStat            `bson:"statistics,omitempty"`
+	EventAt      time.Time              `bson:"event_at,omitempty"`
+	Duration     time.Duration          `bson:"duration"`
+	UpdateResult *mongo.UpdateResult    `bson:"update_result,omitempty"`
+	BulkResult   *mongo.BulkWriteResult `bson:"bulk_result,omitempty"`
+}
+
 type JournalEvent struct {
 	ID                    primitive.ObjectID
-	Session               *sitepages.WebSession
+	Session               sitepages.WebSession
 	TargetId              primitive.ObjectID
 	EventCollectionClient *mongo.Collection
-	Messages              []string
-	CreatedAt             time.Time
+	Statistics            []EventStat
+	EventAt               time.Time
 	DoneAt                time.Time
 	UpdateResult          *mongo.UpdateResult
 	BulkResult            *mongo.BulkWriteResult
@@ -27,30 +43,9 @@ func CreateJournalEvent() *JournalEvent {
 	return &JournalEvent{
 		ID:                    primitive.NewObjectID(),
 		EventCollectionClient: data.GetDefaultEventsCollection(),
-		CreatedAt:             time.Now(),
-		Messages:              []string{},
+		EventAt:               time.Now(),
+		Statistics:            []EventStat{},
 	}
-}
-
-func newJournalEvent(targetId primitive.ObjectID, dbCollection *mongo.Collection) *JournalEvent {
-	return &JournalEvent{
-		ID:                    primitive.NewObjectID(),
-		TargetId:              targetId,
-		EventCollectionClient: dbCollection,
-		CreatedAt:             time.Now(),
-		Messages:              []string{},
-	}
-}
-
-type EventDocument struct {
-	ID           primitive.ObjectID     `bson:"_id,omitempty"`
-	SessionId    primitive.ObjectID     `bson:"session_id"`
-	TargetId     primitive.ObjectID     `bson:"target_id"`
-	Messages     []string               `bson:"messages"`
-	CreatedAt    time.Time              `bson:"created_at"`
-	Duration     time.Duration          `bson:"duration"`
-	UpdateResult *mongo.UpdateResult    `bson:"update_result"`
-	BulkResult   *mongo.BulkWriteResult `bson:"bulk_result"`
 }
 
 func (e *JournalEvent) SetTargetId(targetId primitive.ObjectID) {
@@ -61,7 +56,8 @@ func (e *JournalEvent) SetEventCollection(dbCollection *mongo.Collection) {
 	e.EventCollectionClient = dbCollection
 }
 
-func (e *JournalEvent) SetSession(session *sitepages.WebSession) {
+func (e *JournalEvent) SetSession(session sitepages.WebSession) {
+	LogSession(session)
 	e.Session = session
 }
 
@@ -73,24 +69,24 @@ func (e *JournalEvent) SetBulkResult(result *mongo.BulkWriteResult) {
 	e.BulkResult = result
 }
 
-func (e *JournalEvent) AddMessage(message string) {
-	log.Printf("journal (%s) msg: %s", e.ID.Hex(), message)
-	e.Messages = append(e.Messages, message)
+func (e *JournalEvent) AddStat(statKey string, info string) {
+	log.Printf("journal (%s) key: %s, msg: %s", e.ID.Hex(), statKey, info)
+	e.Statistics = append(e.Statistics, EventStat{
+		Key:  statKey,
+		Info: info,
+	})
 }
 
 func (e *JournalEvent) ToDocument() EventDocument {
 	done := time.Now()
-	sessionId := primitive.NilObjectID
-	if e.Session != nil {
-		sessionId = e.Session.ID
-	}
+
 	return EventDocument{
 		ID:           e.ID,
-		SessionId:    sessionId,
+		SessionId:    e.Session.ID,
 		TargetId:     e.TargetId,
-		Messages:     e.Messages,
-		CreatedAt:    e.CreatedAt,
-		Duration:     done.Sub(e.CreatedAt),
+		Statistics:   e.Statistics,
+		EventAt:      e.EventAt,
+		Duration:     done.Sub(e.EventAt),
 		UpdateResult: e.UpdateResult,
 		BulkResult:   e.BulkResult,
 	}
@@ -99,7 +95,7 @@ func (e *JournalEvent) ToDocument() EventDocument {
 func (e *JournalEvent) Done() {
 	doc := e.ToDocument()
 	log.Printf("Journal %s Event Done: %s, Session: %s, Target: %s, Start: %s, Duration micro sec: %d",
-		e.EventCollectionClient.Name(), doc.ID.Hex(), doc.SessionId.Hex(), doc.TargetId.Hex(), doc.CreatedAt, doc.Duration.Microseconds())
+		e.EventCollectionClient.Name(), doc.ID.Hex(), doc.SessionId.Hex(), doc.TargetId.Hex(), doc.EventAt, doc.Duration.Microseconds())
 	e.EventCollectionClient.InsertOne(context.Background(), doc)
 
 }
