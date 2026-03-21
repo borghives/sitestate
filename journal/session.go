a45207ded4d966b160c92344a81884f2002cd104
Ed Dao <ed@intellogic.com>
2026-03-21 06:43:23 -0700

    fix

--- journal/session.go
+++ journal/session.go
@@ -12,6 +12,12 @@ import (
 
 var DB_SESSION_INFO_COLLECTION_NAME = "session_info"
 
+var webStore data.Web
+
+func InitializeWebEvent(store data.Web) {
+	webStore = store
+}
+
 func LogSession(session websession.Session) {
 	opt := options.UpdateOne().SetUpsert(true)
 	update := data.NewUpdate().SetDoc(session).SetOnInsert("event_at", time.Now()).CurrentDate("last_seen")
